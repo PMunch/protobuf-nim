fbebbab8582e612d870e6f52e00875562f86ef4a
PMunch <peterme@peterme.net>
2019-09-23 10:28:37 +0200

    Fix paths now relative to root directory

--- tests/tparse.nim
+++ tests/tparse.nim
@@ -2,5 +2,7 @@ import "../src/protobuf"
 
 # Check to see that everything parses. Should add some more testing to verify
 # that it actually creates the correct result
-parseProtoFile("parse.prot")
+# Note that when running `nimble test` the starting directory is the root
+# directory of the repository for file reads (but not for imports)
+parseProtoFile("tests/parse.prot")
 echo "All good!"
